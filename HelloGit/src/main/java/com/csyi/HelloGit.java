bdeb65720e4ba2c0106ad1b90d8c3dec676ee053
wufq <1832785141@qq.com>
2021-01-11 17:53:00 +0800

    dev分支提交内容

--- HelloGit/src/main/java/com/csyi/HelloGit.java
+++ HelloGit/src/main/java/com/csyi/HelloGit.java
@@ -5,9 +5,19 @@ package com.csyi;
  * @Date 2021/1/11 16:25
  */
 public class HelloGit {
+    private static int sum = 0;
+
     public static void main(String[] args){
+
+
         System.out.println("git");
         System.out.println("更新");
         System.out.println("更新5");
+
+        for(int i=0;i<5;i++){
+            sum += i;
+        }
+
+        System.out.println("sum="+sum);
     }
 }
