0ce5ddbf2724db3ecd9b7fff44531465cb97acff
wufq <1832785141@qq.com>
2021-01-11 17:05:59 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/csyi/HelloGit.java
@@ -0,0 +1,11 @@
+package com.csyi;
+
+/**
+ * @Author wufq
+ * @Date 2021/1/11 16:25
+ */
+public class HelloGit {
+    public static void main(String[] args){
+        System.out.println("git");
+    }
+}
